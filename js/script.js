7545e0755e050e4bdddf5562f5da20accf54e68e
Vedansh Bhartia <vedanshbhartia@gmail.com>
2018-02-27 01:55:57 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", async function() {
+
+	document.getElementById('hr-1').style.marginLeft = "22.5%";
+	document.getElementById('hr-2').style.marginRight = "22.5%";
+	console.log("lfsdjk");
+});
